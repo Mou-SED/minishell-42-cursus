c3b114cac07df1f32342680034bdd65aafec93e8
Mou-SED <moseddik@student.1337.ma>
2022-06-26 20:49:06 +0100

    feat: Add prompt

--- src/main/main.c
+++ src/main/main.c
@@ -6,7 +6,7 @@
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 10:48:30 by moseddik          #+#    #+#             */
-/*   Updated: 2022/06/24 10:02:21 by moseddik         ###   ########.fr       */
+/*   Updated: 2022/06/26 20:07:24 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,15 @@ int main(int ac, char **av)
 {
 	(void)ac;
 	(void)av;
-	scanner();
-	tokenizer();
-	parsing();
-	printf("minishell is loading...\n");
+	char *cmd;
+
+	while (1)
+	{
+		cmd = readline("\001\e[1;36m\002MiniShell~ \001\e[0m\002 ");
+		if (cmd == NULL)
+		{
+			printf("exit\n");
+			break ;
+		}
+	}
 }
