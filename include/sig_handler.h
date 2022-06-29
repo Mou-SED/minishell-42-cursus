1351fb9af0948a2606963431e0c09a30b96ea2ba
Ziko909 <zikoziko078@gmail.com>
2022-06-29 21:49:52 +0100

    exp: Add handlers for C^d and C^\

--- include/sig_handler.h
+++ include/sig_handler.h
@@ -1,27 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   sig_handler.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zaabou <zaabou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/18 10:46:59 by moseddik          #+#    #+#             */
-/*   Updated: 2022/06/29 21:48:38 by zaabou           ###   ########.fr       */
+/*   Created: 2022/06/29 15:34:03 by zaabou            #+#    #+#             */
+/*   Updated: 2022/06/29 21:48:59 by zaabou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+# ifndef SIG_HANDLER_H
+# define SIG_HANDLER_H
 
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include "readline/readline.h"
-# include "readline/history.h"
-# include "sig_handler.h"
+# include <signal.h>
 
-void	scanner(void);
-void	tokenizer(void);
-void	parsing(void);
-
+void    scan_input(int sig);
+void	ctl_c_handle();
+void	ctl_d_handler(char *cmd);
+void	ctl_backslash_handle(void);
+void	handel_signals(void);
 # endif
--- src/main/main.c
+++ src/main/main.c
@@ -3,28 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: zaabou <zaabou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 10:48:30 by moseddik          #+#    #+#             */
-/*   Updated: 2022/06/26 20:07:24 by moseddik         ###   ########.fr       */
+/*   Updated: 2022/06/29 21:48:04 by zaabou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main(int ac, char **av)
+void	ctl_d_handler(char *cmd)
 {
+	if (cmd == NULL)
+	{
+		printf("\x1B[1;A\x1B[11;Cexit\n");
+		exit(EXIT_SUCCESS);
+	}
+}
+
+void	ctl_backslash_handle(void)
+{
+	signal(SIGQUIT, SIG_IGN);
+}
+
+void	handel_signals(void)
+{
+	ctl_backslash_handle();
+}
+
+int	main(int ac, char **av)
+{
+	char	*cmd;
+
 	(void)ac;
 	(void)av;
-	char *cmd;
-
+	handel_signals();
 	while (1)
 	{
-		cmd = readline("\001\e[1;36m\002MiniShell~ \001\e[0m\002 ");
-		if (cmd == NULL)
-		{
-			printf("exit\n");
-			break ;
-		}
+		cmd = readline("\001\e[1;36m\002MiniShell~ \001\e[0m\002");
+		ctl_d_handler(cmd);
 	}
 }
