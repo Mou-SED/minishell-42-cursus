045018c4c9c9b69415903f590be463b3677ee47a
Mou-SED <moussa.seddik.ph@gmail.com>
2022-08-01 15:06:42 +0100

    fix(Libft) : Protect ft_strdup

--- lib/libft/src/mandatory/ft_strdup.c
+++ lib/libft/src/mandatory/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 01:31:24 by moseddik          #+#    #+#             */
-/*   Updated: 2022/07/01 17:01:57 by moseddik         ###   ########.fr       */
+/*   Updated: 2022/08/01 11:47:36 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strdup(const char *s1)
 	int		i;
 	char	*ptr;
 
+	if (s1 == NULL)
+		return (NULL);
 	ptr = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
 	i = 0;
 	if (!ptr)
