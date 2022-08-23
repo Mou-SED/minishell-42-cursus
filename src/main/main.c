dfc64b1d4703d965756c71839c2c2aab746be5ca
moseddik <moussa.seddik.ph@gmail.com>
2022-08-23 10:56:54 +0100

    refactor(main): Update main with last changes

--- src/main/main.c
+++ src/main/main.c
@@ -3,24 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zaabou <zaabou@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 10:48:30 by moseddik          #+#    #+#             */
-/*   Updated: 2022/08/20 23:13:11 by zaabou           ###   ########.fr       */
+/*   Updated: 2022/08/23 10:55:34 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+void	allocate_memory(t_token_list ***head, t_env ***m_env, char **env, char **pwd)
+{
+	*head = ft_calloc(1, sizeof(t_token_list *));
+	if (*head == NULL)
+		allocation_faild();
+	*m_env = ft_calloc(1, sizeof(t_env *));
+	if (*m_env == NULL)
+		allocation_faild();
+	**m_env = ft_calloc(1, sizeof(t_env));
+	if (**m_env == NULL)
+		allocation_faild();
+	*pwd = getcwd(NULL, 0);
+	build_env(**m_env, env);
+}
+
 void	waiting_for_my_children(void)
 {
+	signal(SIGINT, SIG_IGN);
 	while (wait(&status) != -1)
 	{
         if (WIFEXITED(status))
             status = WEXITSTATUS(status);
         else if (WIFSIGNALED(status))
+		{
+			printf("\n");
             status = WTERMSIG(status) + 128;
+		}
     }
+	signals_handler();
 }
 
 void	wait_for_one_child(pid_t pid)
@@ -38,14 +58,12 @@ int	main(int ac, char **av, char **env)
 	t_token_list	**head;
 	t_ast			*root;
 	t_env			**m_env;
+	char			*cwd;
 	int				i;
 
 	(void)ac;
 	(void)av;
-	
-	head = ft_calloc(1, sizeof(t_token_list *));
-	m_env = ft_calloc(1, sizeof(t_env *));
-	(*m_env = ft_calloc(1, sizeof(t_env)), build_env(*m_env, env));
+	allocate_memory(&head, &m_env, env, &cwd);
 	signals_handler();
 	while (1)
 	{
@@ -55,21 +73,19 @@ int	main(int ac, char **av, char **env)
 			(add_history(cmd), *head = ft_lstnew_token(NULL));
 			(tokenizer(skip_space(cmd), head), free(cmd));
 			if ((*head)->lexeme == NULL || (check_syntax_error(*head, &i) == true
-					&& her_doc(*head, &i) == true))
+					&& her_doc(*head, &i, *m_env) == true))
 			{
-				status = 258;
-				signals_handler();
-				ft_lstclear_tokens(head, &free);
+				(signals_handler(), ft_lstclear_tokens(head, &free));
 				continue ;
 			}
 			root = building_ast(root, *head, m_env);
-			execution(root);
+			execution(root, &cwd);
 			(ft_lstclear_tokens(head, &free), clear_ast(root));
 			waiting_for_my_children();
-			// system("leaks minishell");
+
 		}
 		else if (cmd == NULL)
-			ctl_d_handler(head);
+			ctl_d_handler(head, m_env);
 		else if (*cmd == '\0')
 			free(cmd);
 	}
