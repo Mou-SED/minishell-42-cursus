8c7799644923961df5c6a25eba5624caa9dcabff
moseddik <moussa.seddik.ph@gmail.com>
2022-09-11 19:41:07 +0100

    fix(execution): Fix error out if is directory

--- src/execution/child_work.c
+++ src/execution/child_work.c
@@ -3,14 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   child_work.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zaabou <zaabou@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 15:22:07 by zaabou            #+#    #+#             */
-/*   Updated: 2022/09/06 09:52:40 by zaabou           ###   ########.fr       */
+/*   Updated: 2022/09/11 19:39:59 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+static void	is_dir(t_ast *node)
+{
+	bool	ret;
+	char	*tmp;
+
+	ret = false;
+	tmp = ft_strdup(node->cmd_node->cmd_args);
+	if (ft_strchr(tmp, '/') != NULL)
+		ret = true;
+	free(tmp);
+	if (errno != ENOENT && ret == true)
+	{
+		dup2(STDERR_FILENO, STDOUT_FILENO);
+		printf("\x1b[31m Minshell : %s: Is a directory\n\x1b[0m",
+			node->cmd_node->cmd_table[0]);
+		exit(126);
+	}
+	else if (errno == ENOENT && ret == true)
+	{
+		dup2(STDERR_FILENO, STDOUT_FILENO);
+		printf("\x1b[31m Minshell : %s: No such file or directory\n\x1b[0m",
+			node->cmd_node->cmd_table[0]);
+		exit(127);
+	}
+	else
+		return ;
+}
 
 void	exec_cmd(t_ast *node)
 {
@@ -26,6 +54,7 @@ void	exec_cmd(t_ast *node)
 	unused_pipe(node);
 	env = convert_envirenment(*node->cmd_node->m_env);
 	execve(node->cmd_node->cmd_args, node->cmd_node->cmd_table, env);
+	is_dir(node);
 	dup2(STDERR_FILENO, STDOUT_FILENO);
 	printf("\x1b[31m Minshell : %s: command not found\n\x1b[0m",
 		node->cmd_node->cmd_table[0]);
